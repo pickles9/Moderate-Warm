15d8bf08bc5aef20da996e8bb8ea549f5a148084
pickle <55500841+pickles9@users.noreply.github.com>
2020-05-18 12:25:51 -0400

    bug fixes
    Added score counter, prevented player from spamming inputs, removed bug where player could gain points after game ended.

--- ModerateWarm.py
+++ ModerateWarm.py
@@ -1,60 +1,70 @@
+#GAME MODIFIERS
+speed = 9
+distance = 20 #distance sprites moves on each game tick
+score = 0 #player's score
+enemy_count = 10 #number of enemies spawned
+
+#imports
 import turtle as trtl
-import random as rand
+import random as rand #for random relocating
+
 #configures player turtle
 player = trtl.Turtle() #makes player turtle
 player.pu()
-player.speed(0)
+player.speed(speed)
 player.shape("square")
 player.fillcolor("green")
 player.goto(0,-60)
 
-#configures score tracker    ADD SCORE COUNTER
+#configures score printer
 scoreBot = trtl.Turtle()
 scoreBot.ht()
 scoreBot.pu()
+scoreBot.color("white")
+scoreBot.goto(-270,270)
+font = ("Arial", 20, "normal")
 
-``````
 #configures window
 wn = trtl.Screen() #makes screen variable
 wn.bgcolor("black")
 wn.title("Try and Survive") #renames window
 screen_border = 260
-enemy_count = 9
+
 
 #variable definitions
-distance = 20 #distance sprites moves on each game tick
-score = 0
-
-
-enemies = []
+ready = True #signals when game has made all calculations
+gameactive = True #prevents players from scoring points after game ends
+enemies = [] #list of enemies
 
 
 #sets up movement controls for player
 def up():
-    if player.ycor() <= screen_border:
+    if (player.ycor() <= screen_border) and ready:
         player.sety(player.ycor()+distance)
         game_update()
 def right():
-    if player.xcor() <= screen_border:
+    if (player.xcor() <= screen_border) and ready:
         player.setx(player.xcor()+distance)
         game_update()
 def down():
-    if player.ycor() >= -screen_border:
+    if (player.ycor() >= -screen_border) and ready:
         player.sety(player.ycor()-distance)
         game_update()
 def left():
-    if player.xcor() >= -screen_border:
+    if (player.xcor() >= -screen_border) and ready:
         player.setx(player.xcor()-distance)
         game_update()
 
-def game_update():
-    for enemy in enemies:
+def game_update(): #runs each time player moves
+    global ready
+    ready = False
+    for enemy in enemies: #itterates through each enemy and moves them forward
         enemy.forward(distance)
         hit_detection(enemy)
     update_score()
+    ready = True
 
-def hit_detection(enemy):
-    #print(enemy.distance(player.xcor(), player.ycor()))
+def hit_detection(enemy): #checks to see if sprite is touching player or wall
     if (enemy.distance(player.xcor(), player.ycor()) < 20):
         print("collision")
         end_game()
@@ -65,6 +75,7 @@ def hit_detection(enemy):
 def relocate(sprite): #moves sprite to new location on the border of the screen
     new_side = rand.randint(0,3) #chooses side that sprite will enter from
     new_loc = distance*(rand.randint(-int(screen_border/distance), int(screen_border/distance))) #chooses where on that side the sprite will enter
+    sprite.speed(0)
 
     if new_side == 0:
         sprite.goto(new_loc, screen_border)
@@ -81,9 +92,11 @@ def relocate(sprite): #moves sprite to new location on the border of the screen
     else:
         sprite.goto(-screen_border, new_loc)
         sprite.setheading(0)
+    sprite.speed(speed)
 
 def end_game():
-    print(score)
+    global gameactive
+    gameactive = False
     #Disables player movement
     wn.onkey(None, "Up") 
     wn.onkey(None,"Right")
@@ -95,20 +108,24 @@ def end_game():
         enemy.ht()
         enemy.goto(500, 550)
 
-print(0/0
+print(0/0 add game over screen
 def update_score():
     global score
-    score += 1
+    if gameactive:
+        score += 1
+        scoreBot.clear()
+        scoreBot.write(score,font=font,align="center")
 
-#TESTING ENEMY COLLISION
-for i in range(0,enemy_count):
-    enemies.append(trtl.Turtle())
-for enemy in enemies:
+#Enemy Spawner
+for i in range(0,enemy_count): #moves all enemies into a list for easy management
+    enemies.append(trtl.Turtle()) 
+
+for enemy in enemies: #Configures enemy and moves in somewhere on the map
     enemy.pu()
     enemy.shape("square")
-    enemy.right(90)
+    #enemy.right(90)
     enemy.fillcolor("red")
-    enemy.speed(0)
+    enemy.speed(speed)
     relocate(enemy)
 
 #controls for player
